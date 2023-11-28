cf40f2c1ab9a5b9146f2079ebae47f6d3c87bd79
Brijesh Lakhani <brijesh.lakhani@techivies.com>
2023-11-28 16:45:28 +0530

    mongo config

--- core/config.py
+++ core/config.py
@@ -14,21 +14,3 @@ def get_db_config():
             "MONGO_CONNECTION_URL": os.getenv("MONGO_CONNECTION_URL", "mongodb://localhost:27017/demo_python"),
         }
     return config
-
-
-def get_authentication_url():
-    if env == ".config":
-        config = ConfigParser()
-        config.read(".config")
-        if "AUTH_URL" in config:
-            config = config["AUTH_URL"]
-        else:
-            config = {
-                "URL": os.getenv("URL", "https://something.com"),
-            }
-
-    else:
-        config = {
-            "URL": os.getenv("URL", "https://something.com"),
-        }
-    return config
